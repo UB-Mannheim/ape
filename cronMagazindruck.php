078fe786db123f9dd8d46597f98e3e046cd22ace
root <root@mail.bib.uni-mannheim.de>
2017-10-02 11:01:25 +0200

    Do not print magazine job on public holiday

--- cronMagazindruck.php
+++ cronMagazindruck.php
@@ -1,5 +1,12 @@
 <?php
 
+    $now = getdate();
+    print_r($now);
+    if ($now['mon'] == 10 && $now['mday'] == 3) {
+        echo("Feiertag, kein Magazindruck\n");
+        exit(1);
+    }
+
     include ("printJob.class.php");
 
     $print = new printJob("", "cronMagazindruck");
